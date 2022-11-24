1c6c50fa617934453a08de53a43c20900b6c1eec
Ayman Aoubaida <aoubaida.ayman@gmail.com>
2022-11-24 17:25:20 +0100

    24 nov

--- CodeC.c
+++ CodeC.c
@@ -2,7 +2,56 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct *rame = { int id ; rame* 
+typedef struct node
+{
+    int id ;            
+    struct objet* prec ;    
+    struct objet* suiv ;    
+} node ;
 
 
-typedef struct rame rame;
+typedef node* listecirculaire ; 
+
+
+
+node* creer(int ido){
+  node* l = (node*) malloc(sizeof(node)) ;
+  fst -> id = ido ;
+  fst -> prec = fst ;
+  fst -> suiv = fst ;
+  return l ;
+}
+
+void ajouter(node* l, int x){
+  while(x =< l->id){
+    l = l -> suiv ;
+  }
+  node* new = creer(x);
+  node* p = l -> prec;
+  l -> prec =  new ;
+  p -> suiv = new ;
+  new -> prec = p ;
+  new -> suiv = l ;
+}
+
+void affiche(node* l){
+  while(l -> id =< l -> suiv -> id){
+    printf("%d",l->id) ;
+    l = l -> suiv ;
+  }
+}
+
+
+
+
+
+
+/* liste circulaire doublement chainée pour les rames // pour les stations, 
+
+Remplacer plus tard id par un type correspondant contenant une id, un état/statut 
+
+
+
+*/
+
+
